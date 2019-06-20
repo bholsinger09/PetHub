062101fd918dd972a03b0cd9f51c83d4b2fd342f
Rob Schopke <rob@schopke.com>
2019-06-20 09:58:05 -0600

    Search Menu Item

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -8,6 +8,7 @@
 <script>
   // @ is an alias to /src
   import Carousel from '@/components/Carousel.vue'
+  import FeaturedPets from '@/components/Carousel.vue'
 
   export default {
     name: 'home',
