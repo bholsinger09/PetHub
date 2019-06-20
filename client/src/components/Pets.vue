741da8c3f66c1b1c0565cee960314f5256c3d367
Daniel Stevens <Daniel.Stevens300@Gmail.com>
2019-06-20 11:23:41 -0600

    checkbox

--- client/src/components/Pets.vue
+++ client/src/components/Pets.vue
@@ -4,11 +4,30 @@
     <form @submit.prevent="petSearch" class="searchForm">
       <i class="fas fa-cat"></i> <i class="fas fa-dog"></i>
       <input type="text" v-model="searchParams.dogOrCat" placeholder="dog or cat">
+
       <input type="text" v-model="searchParams.breed" placeholder="breed">
+
       <input type="text" v-model="searchParams.age" placeholder="age">
+
+      <div id='age' v-model="searchParams.age">
+        <input type="checkbox" id="baby" value="baby" v-model="checkedAge">
+        <label for="Baby">Baby</label>
+        <input type="checkbox" id="Young" value="Young" v-model="checkedAge">
+        <label for="Young">Young</label>
+        <input type="checkbox" id="Adult" value="Adult" v-model="checkedAge">
+        <label for="Adult">Adult</label>
+        <input type="checkbox" id="Seinor" value="Seinor" v-model="checkedAge">
+        <label for="Seinor">Seinor</label>
+        <br>
+        <!-- <span>Checked Age: {{ checkedAge }}</span> -->
+      </div>
+
       <input type="text" v-model="searchParams.size" placeholder="size">
+
       <input type="text" v-model="searchParams.gender" placeholder="gender">
+
       <input type="text" v-model="searchParams.location" placeholder="postal code">
+
       <button type="submit">Find</button>
     </form>
 
