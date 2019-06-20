83dab3ffb6597cfdae58b9e384e3ff3e98eb7836
Rob Schopke <rob@schopke.com>
2019-06-20 10:38:30 -0600

    Mid Morning Push

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,18 +2,24 @@
   <div class="home">
 
     <carousel />
+    <featured-pets />
   </div>
 </template>
 
 <script>
   // @ is an alias to /src
   import Carousel from '@/components/Carousel.vue'
-  import FeaturedPets from '@/components/Carousel.vue'
+  import FeaturedPets from '@/components/FeaturedPets.vue'
 
   export default {
     name: 'home',
     components: {
-      Carousel
-    }
+      Carousel,
+      FeaturedPets,
+    },
+    mounted() {
+      this.$store.dispatch('getFeaturedPets')
+    },
+
   }
 </script>
