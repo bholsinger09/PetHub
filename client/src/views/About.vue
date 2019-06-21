ae8e4eeeef6f1cf24999724340b401c06a21d0ee
Daniel Stevens <Daniel.Stevens300@Gmail.com>
2019-06-21 13:35:39 -0600

    About

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>A collaborative project between: Benjamin Holsinger, Danielle Morton, Kelly Phillips, Rob Schopke, and Daniel
+      Stevens. </h2>
+    </br>
+    <h3>Picture Credits</h3>
     <h3>Search - Photo by Krista Mangulsone on Unsplash</h3>
     <h3>Meetup - Photo by Jonas Vincent on Unsplash</h3>
     <h3>Forum - Photo by Aziz Acharki on Unsplash</h3>
