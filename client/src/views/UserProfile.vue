f7d3a5f647fbcb57074ee0ecb71a6c0352eb2e1f
Daniel Stevens <Daniel.Stevens300@Gmail.com>
2019-06-24 15:14:35 -0600

    edit user form

--- client/src/views/UserProfile.vue
+++ client/src/views/UserProfile.vue
@@ -15,18 +15,17 @@
       <div class="row">
         <div class="col-12">
           <h1>Settings</h1>
-          <p>Name:{{user.name}} <form @submit.prevent="editUser" class="">
-              <button class="" type="submit">Edit</button></form>
+          <p>Name:{{user.name}} <form @submit.prevent="editName" class="">
+              <button class="" type="submit"><i class="fas fa-edit"></i></button></form>
           </p>
-          <p>Pic: <img src="User.pic" alt=""> <button @click="">Edit</button>
-            <!--not connected-->
+          <p>Pic: <img src="User.pic" alt="">
+            <form @submit.prevent="editPic" class="">
+              <button class="" type="submit"><i class="fas fa-edit"></i></button>
           </p>
-          <p>email:{{user.email}} <button @click="editUser">Edit</button>
-            <!--not connected-->
-          </p>
-          <p>Location:{{user.zip}} <button @click="editUser">Edit</button>
-            <!--not connected-->
-          </p>
+          <p>Email:{{user.email}} <form @submit.prevent="editEmail" class="">
+              <button class="" type="submit"><i class="fas fa-edit"></i></button></p>
+          <p>Location:{{user.zip}} <form @submit.prevent="editZip" class="">
+              <button class="" type="submit"><i class="fas fa-edit"></i></button></p>
         </div>
       </div>
 
@@ -43,7 +42,22 @@
     // props: ['User'],
     data() {
       return {
-
+        newName: {
+          _id: this._id,
+          name: ''
+        },
+        newPic: {
+          _id: this._id,
+          pic: ''
+        },
+        newEmail: {
+          _id: this._id,
+          email: ''
+        },
+        newZip: {
+          _id: this._id,
+          zip: ''
+        },
       }
 
     },
@@ -56,11 +70,19 @@
       Favorites
     },
     methods: {
-      editUser() {
-        console.log(this.searchParams)
-        this.$store.dispatch('petSearch', this.searchParams)
+      editEmail() {
+        this.$store.dispatch('updateUser', this.newEmail)
       },
-    }
+      editPic() {
+        this.$store.dispatch('updateUser', this.newPic)
+      },
+      editName() {
+        this.$store.dispatch('updateUser', this.newName)
+      },
+      editZip() {
+        this.$store.dispatch('updateUser', this.newZip)
+      },
+    } //methods
 
   }
 
