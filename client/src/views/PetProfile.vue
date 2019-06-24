22273d983711a3e42d9ad40e06e6ac6b19793616
Daniel Stevens <Daniel.Stevens300@Gmail.com>
2019-06-24 10:51:28 -0600

    cleaned PetProfile

--- client/src/views/PetProfile.vue
+++ client/src/views/PetProfile.vue
@@ -7,15 +7,13 @@
     </div>
 
     <div class="col-12 col-sm-6 text-left" style="padding: 30px;">
-      <!-- <h2 v-if="pet.adoptable==false" style="color: #750000">This good boy/girl has found a home.</h2> -->
       <h2 class="card-title">{{pet.name}}</h2>
-      <!-- <p class="card-text">Type: {{pet.type}}</p> -->
       <p class="card-text">Description: {{pet.description}} <a :href="pet.url" target="_blank" class="text-muted">View
           Full Bio</a>
       </p>
       <p class="card-text">Age: {{pet.age}}</p>
       <p class="card-text" v-if="pet.status == 'adoptable'">Status: Adoptable</p>
-      <p class="card-text" v-if="pet.status == 'adopted'">Status: Adopted</p>
+      <p class="card-text" v-if="pet.status == 'adopted'">Status: Adopted</p> <!-- maybe should be red -->
       <p class="card-text" v-if="pet.status == 'found'">Status: Found</p>
       <p class="card-text">Primary Breed: {{pet.breeds.primary}}</p>
       <p class="card-text" v-if="pet.breeds.secondary">Secondary Breed: {{pet.breeds.secondary}}</p>
@@ -35,39 +33,9 @@
         <span class="card-text" v-if="pet.environment.children ==true">• Children </span>
         <span v-if="pet.environment.dogs == true"> • Dogs</span>
         <span v-if="pet.environment.cats == true"> • Cats</span>
-        <!-- <div v-if="pet.gender==female">Spayed: <p> {{pet.attributes.spayed_neutered}} </p> -->
-        <!-- </div> -->
+
     </div>
 
-    <!-- set2 -->
-    <!-- <div class="col-6 col-sm-12">
-      <p class="card-text">Age:{{pet.age}} Sex:{{pet.gender}} Size:{{pet.size}}</p>
-      <p v-if="!pet.breeds.unknown">Breed: {{pet.breeds.primary}} {{pet.breeds.secondary}} {{pet.breeds.mixed}} </p>
-    </div>
-    <div class="col-6 col-sm-12">
-      <h2>Medical</h2> -->
-    <!-- all attributes are boolean -->
-    <!-- <p v-if="pet.gender==male">Neutered: <p> {{pet.attributes.spayed_neutered}} </p>
-      </p>
-      <p v-if="pet.gender==female">Spayed: <p> {{pet.attributes.spayed_neutered}} </p>
-      </p>
-      <p v-if="pet.breed=cat">Declawed: {{pet.attributes.declawed}} </p>
-      <p>House Trained: {{pet.attributes.house_trained}}</p>
-      <p>Special Needs: {{pet.attributes.special_needs}}</p>
-      <p>Shots Current: {{pet.attributes.shots_current}}</p> -->
-    <!-- environment is boolean -->
-    <!-- <p v-if="pet.environment.children || pet.environment.children || pet.environment.dogs">Good With:
-        {{pet.environment.children}} {{pet.environment.cats}} {{pet.environment.dogs}}</p>
-      <p v-else>A home without children or other pets. </p>
-    </div> -->
-    <!-- set3 -->
-    <!-- <div class="col-12"> -->
-    <!-- Contact -->
-    <!-- <p>{{contact.email}} | {{contact.phone}}</p>
-      <p>{{contact.address.address1}} {{contact.address.address2}} {{contact.address.city}},
-        {{contact.address.state}} {{contact.address.postcode}} {{contact.address.country}}</p>
-    </div>
-    <a href="#" class="btn btn-primary">Favorite</a> -->
   </div>
   <div v-else>LOADING...</div>
 </template>
