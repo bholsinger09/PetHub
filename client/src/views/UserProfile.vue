cdc2595cf0380bab5526bcb004cb15a8c3ba593e
Daniel Stevens <Daniel.Stevens300@Gmail.com>
2019-06-24 14:11:56 -0600

    user basic layout

--- client/src/views/UserProfile.vue
+++ client/src/views/UserProfile.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
+    <div class="container-fluid">
+      <div class="row">
+        <h1>Favorites</h1>
+        <div class="col-6"></div>
+        <div class="col-6">
+          <h1>Searches</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <h1>Settings</h1>
+          <p>Name:{{user.name}}</p>
+          <p>Pic:{{user.pic}}</p>
+          <p>email:{{user.email}}</p>
+          <p>Location:{{user.zip}}</p>
+        </div>
+      </div>
+    </div>
     <favorites />
+
   </div>
 </template>
 
