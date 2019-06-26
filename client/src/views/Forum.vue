636a30232a2d98691b1f4095e096a1fe60c44de7
Rob Schopke <rob@schopke.com>
2019-06-26 13:55:52 -0600

    Pushing Mid Topic Rendering

--- client/src/views/Forum.vue
+++ client/src/views/Forum.vue
@@ -2,9 +2,9 @@
   <div class="container">
     <div class="row">
       <div class="col-12" v-for="topic in topics" :key="topic._id">
-        <router-link :to="{name:'topic', params:{topicId:topic._id}}">
-          <h4 id="topic" @click="setActiveTopic(topicId)">{{topic.title}}</h4>
-        </router-link>
+
+        <h4 id="topic" @click="goToTopic(topic._id)">{{topic.title}}</h4>
+
       </div>
     </div>
   </div>
@@ -31,9 +31,9 @@
     },//computed
 
     methods: {
-      setActiveTopic(topicId) {
+      goToTopic(id) {
         debugger
-        this.$store.dispatch("setActiveTopic", topicId)
+        this.$store.dispatch("getTopic", id)
       }
     }, //methods
 
