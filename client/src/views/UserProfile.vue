401011e02097ab8104f474d687d836f73afec1b6
Rob Schopke <rob@schopke.com>
2019-07-12 11:53:56 -0600

    Removed Debuggers

--- client/src/views/UserProfile.vue
+++ client/src/views/UserProfile.vue
@@ -20,26 +20,26 @@
           <h1>Settings</h1>
           <div class="row">
 
-            <div class="col-6">
+            <div class="col-3">
               <p>Pic: <img src="User.pic" alt="">
                 <form @submit.prevent="editPic" class=""><input type="text" v-model="newPic.pic" placeholder="Pic Link">
                   <button class="" type="submit"><i class="fas fa-edit"></i></button>
               </p>
               </form>
             </div><!-- col-6 pic-->
-            <div class="col-6">
+            <div class="col-3">
               <p>Name:{{user.name}} <form @submit.prevent="editName" class=""><input type="text" v-model="newName.name"
                     placeholder="Name">
                   <button class="" type="submit"><i class="fas fa-edit"></i></button></form>
               </p>
             </div><!-- col-6 name-->
-            <div class="col-6">
+            <div class="col-3">
               <p>Email:{{user.email}} <form @submit.prevent="editEmail" class=""><input type="email"
                     v-model="newEmail.Email" placeholder="E-mail">
                   <button class="" type="submit"><i class="fas fa-edit"></i></button></p>
               </form>
             </div><!-- col-6 email-->
-            <div class="col-6">
+            <div class="col-3">
               <p>Location:{{user.zip}} <form @submit.prevent="editZip" class=""><input type="number"
                     v-model="newZip.zip" placeholder="zip code">
                   <button class="" type="submit"><i class="fas fa-edit"></i></button></p>
