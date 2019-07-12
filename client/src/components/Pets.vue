61ed5594bfd3004ddbbfd040001c236cd99c733f
Daniel Stevens <Daniel.Stevens300@Gmail.com>
2019-07-12 14:52:57 -0600

    pets search buttons

--- client/src/components/Pets.vue
+++ client/src/components/Pets.vue
@@ -1,67 +1,72 @@
 <template>
   <div id="pets-results">
-    <br>
+    <br></br>
 
     <form @submit.prevent="petSearch" class="searchForm container-fluid">
-      <div class="row">
+      <div class="row d-flex justify-content-left">
 
         <!-- <input type="text" v-model="searchParams.type" placeholder="dog or cat"> -->
-        <div class=" col-md-2 col-sm-6" id='type'>
+        <div class="col-md-2 col-sm-6 text-left" id='type'>
           <h6>Animal:
             <input type="radio" class="hidden" id="Dog" value="Dog" v-model="searchParams.type">
-            <label for="Dog"><i class="fas fa-dog fa-lg" @click="activeDog = !activeDog"
+            <label for="Dog" class="btn-sm btn-success"> <i class="fas fa-dog fa-lg" @click="activeDog = !activeDog"
                 :class="{selected: activeDog}"></i></label>
             <input type="radio" class="hidden" id="Cat" value="Cat" v-model="searchParams.type">
-            <label for="Cat"><i class="fas fa-cat fa-lg" @click="activeCat = !activeCat"
+            <label for="Cat" class="btn-sm btn-success"><i class="fas fa-cat fa-lg" @click="activeCat = !activeCat"
                 :class="{selected: activeCat}"></i></label>
           </h6>
         </div>
 
 
         <!-- <input type="text" v-model="searchParams.age" placeholder="age"> -->
-        <div class=" col-md-3  col-sm-6" id='age'>
+        <div class=" col-md-3  col-sm-6 text-left" id='age'>
           <!-- <p>{{searchParams.age}}</p> -->
           <h6>Age: <input type="checkbox" class="hidden" id="Baby" value="Baby" v-model="searchParams.age">
-            <label for="Baby" @click="activeBaby = !activeBaby" :class="{selected: activeBaby}">Baby</label>
+            <label for="Baby" class="btn-sm btn-success" @click="activeBaby = !activeBaby"
+              :class="{selected: activeBaby}">Baby</label>
             <input type="checkbox" class="hidden" id="Young" value="Young" v-model="searchParams.age">
-            <label for="Young" @click="activeYoung = !activeYoung" :class="{selected: activeYoung}">Young</label>
+            <label for="Young" class="btn-sm btn-success" @click="activeYoung = !activeYoung"
+              :class="{selected: activeYoung}">Young</label>
             <input type="checkbox" class="hidden" id="Adult" value="Adult" v-model="searchParams.age">
-            <label for="Adult" @click="activeAdult = !activeAdult" :class="{selected: activeAdult}">Adult</label>
+            <label for="Adult" class="btn-sm btn-success" @click="activeAdult = !activeAdult"
+              :class="{selected: activeAdult}">Adult</label>
             <input type="checkbox" class="hidden" id="Senior" value="Senior" v-model="searchParams.age">
-            <label for="Senior" @click="activeSenior = !activeSenior" :class="{selected: activeSenior}">Senior</label>
+            <label for="Senior" class="btn-sm btn-success" @click="activeSenior = !activeSenior"
+              :class="{selected: activeSenior}">Senior</label>
           </h6>
         </div>
 
         <!-- <input type="text" v-model="searchParams.gender" placeholder="gender"> -->
         <div class="col-md-2 col-sm-6 text-left" id='gender'>
           <h6>Gender: <input type="checkbox" class="hidden" id="male" value="male" v-model="searchParams.gender">
-            <label for="male"><i class="fas fa-mars" @click="activeMars = !activeMars" :class="{selected: activeMars}">
+            <label for="male" class="btn-sm btn-success"><i class="fas fa-mars" @click="activeMars = !activeMars"
+                :class="{selected: activeMars}">
               </i>
             </label>
             <input type="checkbox" class="hidden" id="female" value="female" v-model="searchParams.gender">
-            <label for="female"> <i class="fas fa-venus" @click="activeVenus = !activeVenus"
+            <label for="female" class="btn-sm btn-success"> <i class="fas fa-venus" @click="activeVenus = !activeVenus"
                 :class="{selected: activeVenus}"></i>
             </label></h6>
         </div>
-      </div>
+        <!-- </div> -->
 
-      <!-- <input type="text" v-model="searchParams.size" placeholder="size"> -->
-      <div class="row d-flex justify-content-left">
-        <div class="col-3 col-md-3 col-sm-4 text-left" id='size'>
+        <!-- <input type="text" v-model="searchParams.size" placeholder="size"> -->
+        <!-- <div class="row d-flex justify-content-left"> -->
+        <div class="col-md-3 col-sm-5 text-left" id='size'>
           <input type="checkbox" class="hidden" id="small" value="small" v-model="searchParams.size">
-          <label for="small">
-            Size: <i class="fas fa-cat fa-xs" @click="activeSmall = !activeSmall" :class="{selected: activeSmall}"></i>
+          Size: <label for="small" class="btn-sm btn-success"><i class="fas fa-cat fa-xs"
+              @click="activeSmall = !activeSmall" :class="{selected: activeSmall}"></i>
           </label>
           <input type="checkbox" class="hidden" id="medium" value="medium" v-model="searchParams.size">
-          <label for="medium">
+          <label for="medium" class="btn-sm btn-success">
             <i class="fas fa-dog fa-sm" @click="activeMedium = !activeMedium" :class="{selected: activeMedium}"></i>
           </label>
           <input type="checkbox" class="hidden" id="large" value="large" v-model="searchParams.size">
-          <label for="large">
+          <label for="large" class="btn-sm btn-success">
             <i class="fas fa-cat fa-lg" @click="activeLarge = !activeLarge" :class="{selected: activeLarge}"></i>
           </label>
           <input type="checkbox" class="hidden" id="xlarge" value="xlarge" v-model="searchParams.size">
-          <label for="xlarge">
+          <label for="xlarge" class="btn-sm btn-success">
             <i class="fas fa-dog fa-2x" @click="activeXLarge = !activeXLarge" :class="{selected: activeXLarge}"></i>
 
           </label>
