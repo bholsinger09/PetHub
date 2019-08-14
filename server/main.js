afd7f6122f7c032860db54cbf7c05b558c81547f
Kelly Phillips <fooxiefloof@gmail.com>
2019-08-13 19:07:42 -0600

    commit

--- server/main.js
+++ server/main.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 3000
 server.use(express.static(__dirname + '/../client/dist'))
 
 
-var whitelist = ['http://localhost:8080'];
+var whitelist = ['http://localhost:8080', 'https://pethub-summer19.herokuapp.com/'];
 var corsOptions = {
   origin: function (origin, callback) {
     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
