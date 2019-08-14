b44b7fd95261c0328f25e3e3e6b9914784f921cc
Rob Schopke <rob@schopke.com>
2019-08-14 13:56:00 -0600

    fixes axios base

--- client/src/store.js
+++ client/src/store.js
@@ -5,16 +5,16 @@ import Axios from 'axios'
 
 Vue.use(Vuex)
 
-let base = window.location.host.includes('localhost:8080') ? '//localhost:3000' : '/'
+let base = window.location.host.includes('localhost') ? '//localhost:3000/' : '/'
 
 let api = Axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: base + 'api',
   timeout: 3000,
   withCredentials: true
 })
 
 let auth = Axios.create({
-  baseURL: "http://localhost:3000/api/auth/",
+  baseURL: base + "api/auth",
   // timeout: 3000,
   withCredentials: true
 })
