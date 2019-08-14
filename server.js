419a214d48252eb1e5978013817f9b20746dcf47
Kelly Phillips <fooxiefloof@gmail.com>
2019-08-13 18:38:00 -0600

    Adding File.

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-const express = require('express')
-const port = process.env.PORT || 3000
-const app = express();
-
-app.use(express.static(__dirname + '/../client/dist'))
-
-app.get(/.*/, function (req, res) {
-  res.sendfile(__dirname + "/dist/index.html");
-});
-
+// server.js
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log("Server started!")
+console.log('server started ' + port);
